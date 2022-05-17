8d71c861f5005b54cffbc41efbc9fdaf0ec50699
matte0pisani <matteo.pisani.roma@gmail.com>
2022-05-17 17:08:39 +0200

    Aggiunto metodo per gestire aggiunta film

--- src/main/java/it/uniroma3/siw/controller/FilmController.java
+++ src/main/java/it/uniroma3/siw/controller/FilmController.java
@@ -2,15 +2,20 @@ package it.uniroma3.siw.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import it.uniroma3.siw.model.Film;
+import it.uniroma3.siw.service.FilmService;
 
 @Controller
 public class FilmController {
+	@Autowired
+	private FilmService filmService;
 	
 	@GetMapping("/filmForm")
 	public String getFilmForm(Model model) {
@@ -18,7 +23,13 @@ public class FilmController {
 		return "filmForm.html";
 	}
 	
-	public String addFilm(@Valid Film film, BindingResult bindingResult) {
-		return null;
+	@PostMapping("/film")
+	public String addFilm(@Valid Film film, BindingResult bindingResult, Model model) {
+		if(!bindingResult.hasErrors()) {
+			filmService.addFilm(film);
+			model.addAttribute("film", film);
+			return "film.html";
+		}
+		else return "filmForm.html"; // per ora, se film non esiste, ti riporta a homepage
 	}
 }
