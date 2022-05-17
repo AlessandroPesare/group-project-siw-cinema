e9c2c67d1ad3aef456b44ac41526b1389c1a0c27
AlessandroPesare <ale.pesare@stud.uniroma3.it>
2022-05-17 16:09:02 +0200

    Introdotti attributi getter e setter delle classi del modello

--- src/main/java/it/uniroma3/siw/model/Film.java
+++ src/main/java/it/uniroma3/siw/model/Film.java
@@ -1,5 +1,74 @@
 package it.uniroma3.siw.model;
 
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class Film {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(unique = true)
+	@NotBlank
+	private String titolo;
+	
+	@NotBlank
+	private String descrizione;
+	
+	private List<Sala> sale;
+	
+	@NotBlank
+	private String genere;
+	
+	@NotNull
+	private String regista;
+	
+	public String getGenere() {
+		return genere;
+	}
+
+	public void setGenere(String genere) {
+		this.genere = genere;
+	}
+
+	public String getRegista() {
+		return regista;
+	}
+
+	public void setRegista(String regista) {
+		this.regista = regista;
+	}
+
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public List<Sala> getSale() {
+		return sale;
+	}
+
+	public void setSale(List<Sala> sale) {
+		this.sale = sale;
+	}
 
 }
